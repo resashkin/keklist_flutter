28b147217646140ef1efb84b69c92d3685c968a2
Aleksandr Martseniuk <sashkn2@gmail.com>
2025-07-27 18:26:33 +0200

    Fix android bundle id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.sashkyn.keklist"
+    namespace = "com.sashkyn.emodzen"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.sashkyn.keklist"
+        applicationId = "com.sashkyn.emodzen"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
